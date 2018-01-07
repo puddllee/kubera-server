0c6c3d8b2e3cfc0ef81647e22f1ea29de1463938
Jake Runzer <jakerunzer@gmail.com>
2018-01-06 18:28:11 -0800

    trying something

--- config/config.exs
+++ config/config.exs
@@ -34,7 +34,8 @@ config :ueberauth, Ueberauth,
 config :ueberauth, Ueberauth.Strategy.Google.OAuth,
   client_id: System.get_env("GOOGLE_CLIENT_ID"),
   client_secret: System.get_env("GOOGLE_CLIENT_SECRET"),
-  redirect_uri: System.get_env("GOOGLE_CALLBACK_URL")
+  redirect_uri: System.get_env("GOOGLE_CALLBACK_URL"),
+  callback_url: System.get_env("GOOGLE_CALLBACK_URL")
 
 config :guardian, Guardian,
   allowed_algos: ["HS512"],
