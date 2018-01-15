e5785d319f1dafc561b9bcb4b4adbca0d8886937
Jake Runzer <jakerunzer@gmail.com>
2018-01-14 16:49:01 -0800

    config edit

--- config/prod.exs
+++ config/prod.exs
@@ -16,15 +16,13 @@ use Mix.Config
 config :kubera, KuberaWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: System.get_env("DOMAIN"), port: 80],
-  load_from_system_env: true,
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure the database
 config :kubera, Kubera.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-  ssl: true
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
 # Do not print debug messages in production
 config :logger, level: :info
