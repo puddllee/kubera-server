f3a62ccc6feeba546aea7a5fd64923fc68442553
Jake Runzer <jakerunzer@gmail.com>
2018-01-14 16:57:35 -0800

    add custom procfile

--- config/prod.exs
+++ config/prod.exs
@@ -22,7 +22,8 @@ config :kubera, KuberaWeb.Endpoint,
 config :kubera, Kubera.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  sll: true
 
 # Do not print debug messages in production
 config :logger, level: :info
